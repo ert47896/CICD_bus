b1317375feb860f531dd80dbf993984e9bf8ae10
ert47896 <ert47896@gmail.com>
2023-02-05 16:42:08 +0000

    test

--- flask/startproject.sh
+++ flask/startproject.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd /workspace/flask/app
-python ./module/table_init.py &&  python ./module/MOTCdata_init.py
+python ./module/table_init.py
+sleep 10
+python ./module/MOTCdata_init.py
 uwsgi --ini app.ini
