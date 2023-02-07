c11d520181c3da3ac835fa1d7229ded8d9dd9b94
Wei <ert47896@gmail.com>
2023-02-07 12:21:01 +0800

    Update checkdbdata.sh

--- flask/checkdbdata.sh
+++ flask/checkdbdata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/module/cache.txt && echo 'Success!'")
-while [ ! $(docker exec -i web sh -c "test -f /workspace/flask/app/checkpoint.txt && echo 'Success!'") ];
+#check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/cache.txt && echo 'Success!'")
+while [ ! $(docker exec -i web sh -c "test -f /workspace/flask/app/checkpoint.txt") ];
 do
 	echo "Wait insert TDX data into MySQL"
 	echo $(docker exec -i web sh -c "cd /workspace/flask/app && ls -la")
