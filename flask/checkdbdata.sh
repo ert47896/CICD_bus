0f4cf0c011fc8f00031746fad8f9ecbf11779579
ert47896 <ert47896@gmail.com>
2023-02-07 03:42:00 +0000

    test

---
+++ flask/checkdbdata.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+result="Success!"
+#check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/module/cache.txt" && echo "Success!")
+while [ ! $(docker exec -i web sh -c "test -f /workspace/flask/app/module/checkpoint.txt" && echo "Success!") ];
+do
+	echo "Wait insert TDX data into MySQL"
+	sleep 5
+done
+docker exec -i web sh -c "cd /workspace/flask/app && python -m pytest -v"
+
