a0de3398b3802867ac2677120713e3214ae2c3be
Ubuntu <ert47896@gmail.com>
2023-02-10 05:59:17 +0000

    test no

--- flask/checkdbdata.sh
+++ flask/checkdbdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/cache.txt && echo 'Success!'")
 unfinished=$(docker top web | grep "[p]ython ./module/MOTCdata_init.py")
-dpkg-reconfigure dash -no
+dpkg-reconfigure dash && no
 while [ $unfinished ];
 do
 	echo "Wait insert TDX data into MySQL"
