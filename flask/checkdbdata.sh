27a0ce981f02c2ecd08552dff0065b02f55273d2
Ubuntu <ert47896@gmail.com>
2023-02-10 06:49:36 +0000

    test

--- flask/checkdbdata.sh
+++ flask/checkdbdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/cache.txt && echo 'Success!'")
 unfinished=$(docker top web | grep "[p]ython ./module/MOTCdata_init.py")
-yes n | sudo dpkg-reconfigure dash
+sudo dpkg-reconfigure dash
 cd /bin
 ls -la
 echo $unfinished
