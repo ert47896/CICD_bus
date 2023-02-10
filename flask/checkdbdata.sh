3f006c3f249625fa35b036fa965e8ab8fcc84e29
Ubuntu <ert47896@gmail.com>
2023-02-10 03:25:34 +0000

    test

--- flask/checkdbdata.sh
+++ flask/checkdbdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/cache.txt && echo 'Success!'")
 docker top web | grep "[p]ython ./module/MOTCdata_init.py"
-while [ $(docker exec -i web sh -c "ps aux | grep '[p]ython MOTCdata_init.py' && echo 'Success!'") ];
+while [ $(docker top web | grep "[p]ython ./module/MOTCdata_init.py") ];
 do
 	echo "Wait insert TDX data into MySQL"
 	#echo $(docker exec -i web sh -c "cd /workspace/flask/app && ls -la")
