127a2638de32c65636322c8715764e05b8f9e458
Ubuntu <ert47896@gmail.com>
2023-02-10 08:58:33 +0000

    test

--- flask/checkdbdata.sh
+++ flask/checkdbdata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #check_result=$(docker exec -i web sh -c "test -f /workspace/flask/app/cache.txt && echo 'Success!'")
-unfinished=$(docker top web | grep "[p]ython ./module/MOTCdata_init.py" | awk "{print $1}")
-
+unfinished=$(docker top web | grep "[p]ython ./module/MOTCdata_init.py" | awk "{print $2}")
+echo $unfinished
 cd /bin
 ls -la | grep sh
 echo $unfinished
