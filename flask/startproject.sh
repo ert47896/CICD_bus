a626fd489edd3f2cf04dc4a287cf0655edf0e625
Ubuntu <ert47896@gmail.com>
2023-02-12 09:31:35 +0000

    test

--- flask/startproject.sh
+++ flask/startproject.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /workspace/flask/app
-[ -f checkpoint.txt ] && rm checkpoint.txt
+[ -f checkpoint.txt ] && yes y | rm checkpoint.txt
 python ./module/table_init.py
 python ./module/MOTCdata_init.py
 uwsgi --ini app.ini
